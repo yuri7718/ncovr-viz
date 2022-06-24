4c187802a71abf0f0b72021d5032551067820ed7
yuri7718 <qiangxu1204@gmail.com>
2022-06-24 09:49:35 -0700

    add homepage

---
+++ precache-manifest.01f9adcd464a644d65d2b91934d999d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9fabfd839ec611b40e6",
+    "url": "/ncovr-viz/static/css/main.037d223d.chunk.css"
+  },
+  {
+    "revision": "a9fabfd839ec611b40e6",
+    "url": "/ncovr-viz/static/js/main.a9fabfd8.chunk.js"
+  },
+  {
+    "revision": "f4e761c65dc26d9a876f",
+    "url": "/ncovr-viz/static/js/1.f4e761c6.chunk.js"
+  },
+  {
+    "revision": "e2834dc7c4483ca64058",
+    "url": "/ncovr-viz/static/css/2.04284bc4.chunk.css"
+  },
+  {
+    "revision": "e2834dc7c4483ca64058",
+    "url": "/ncovr-viz/static/js/2.e2834dc7.chunk.js"
+  },
+  {
+    "revision": "edd360bb96ba8c1fdf20",
+    "url": "/ncovr-viz/static/js/runtime~main.edd360bb.js"
+  },
+  {
+    "revision": "c46752c187ebc04c5c009fbf5bea8576",
+    "url": "/ncovr-viz/static/media/NAT_counties.c46752c1.csv"
+  },
+  {
+    "revision": "e22e9ae739a1ab656754cde785de07dd",
+    "url": "/ncovr-viz/static/media/NAT_states.e22e9ae7.csv"
+  },
+  {
+    "revision": "6db895bed9d5b1cb4a1aec8046225861",
+    "url": "/ncovr-viz/static/media/NAT.6db895be.geojson"
+  },
+  {
+    "revision": "2aa3311ddd12924b6ce9de61cf4e96f1",
+    "url": "/ncovr-viz/static/media/US_states.2aa3311d.geojson"
+  },
+  {
+    "revision": "8827bc0a3fa55e319995e548019b7f40",
+    "url": "/ncovr-viz/index.html"
+  }
+];
