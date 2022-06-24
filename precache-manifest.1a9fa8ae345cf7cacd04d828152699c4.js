68cd86dfc19b998ebcf9021ddae13d0960c4363e
yuri7718 <qiangxu1204@gmail.com>
2022-06-24 09:33:28 -0700

    first commit

---
+++ precache-manifest.1a9fa8ae345cf7cacd04d828152699c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9fabfd839ec611b40e6",
+    "url": "/static/css/main.037d223d.chunk.css"
+  },
+  {
+    "revision": "a9fabfd839ec611b40e6",
+    "url": "/static/js/main.a9fabfd8.chunk.js"
+  },
+  {
+    "revision": "f4e761c65dc26d9a876f",
+    "url": "/static/js/1.f4e761c6.chunk.js"
+  },
+  {
+    "revision": "be0520ba5511ac60bd16",
+    "url": "/static/css/2.04284bc4.chunk.css"
+  },
+  {
+    "revision": "be0520ba5511ac60bd16",
+    "url": "/static/js/2.be0520ba.chunk.js"
+  },
+  {
+    "revision": "34b08ee5e21bdfb7c303",
+    "url": "/static/js/runtime~main.34b08ee5.js"
+  },
+  {
+    "revision": "c46752c187ebc04c5c009fbf5bea8576",
+    "url": "/static/media/NAT_counties.c46752c1.csv"
+  },
+  {
+    "revision": "e22e9ae739a1ab656754cde785de07dd",
+    "url": "/static/media/NAT_states.e22e9ae7.csv"
+  },
+  {
+    "revision": "6db895bed9d5b1cb4a1aec8046225861",
+    "url": "/static/media/NAT.6db895be.geojson"
+  },
+  {
+    "revision": "2aa3311ddd12924b6ce9de61cf4e96f1",
+    "url": "/static/media/US_states.2aa3311d.geojson"
+  },
+  {
+    "revision": "6a64ea9a88ec343a5189e7ad4dbc59c2",
+    "url": "/index.html"
+  }
+];
